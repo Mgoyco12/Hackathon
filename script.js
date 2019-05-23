467ce882b23c776ccb03f03a2ccf2a6d3369b61b
Mgoyco12 <43241864+Mgoyco12@users.noreply.github.com>
2019-05-23 09:31:49 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$(".hint").hide();
+$(".go").click(function(){
+    $(".hint").show();
+    
+});
+$(".solve").click(function(){
+   var answer = $(".run").val(); 
+   if(answer==="mom"){
+    $(".cookie").show();
+    $("p").text("you were right");
+   }
+   
+    });
